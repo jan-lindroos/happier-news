c9ac23d98f6414c6a85ea9f8eae5931da7ad0d8f
jan-lindroos <janlindroos4@gmail.com>
2025-08-12 02:54:05 +0300

    chore: fix svelte output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup.html'),
+        inference: resolve(__dirname, 'src/inference/inference.html'),
+        background: resolve(__dirname, 'src/background.ts'),
+        content: resolve(__dirname, 'src/content/content.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      }
+    }
+  },
+  publicDir: resolve(__dirname, 'public')
 })
